b724bdcef40b8247870c93b1ace98a02d00f2805
Advocacy-for-the-Unhoused <madhavsindia@gmail.com>
2026-02-15 00:31:59 -0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,39 @@
+const CACHE_NAME = "afu-cache-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./app.js",
+  "./manifest.json",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(k => k !== CACHE_NAME && caches.delete(k)))
+    )
+  );
+});
+
+self.addEventListener("fetch", event => {
+  const url = new URL(event.request.url);
+
+  // Don't cache API calls to Apps Script
+  if (url.href.includes("script.google.com")) {
+    return;
+  }
+
+  event.respondWith(
+    caches.match(event.request).then(cached => {
+      return cached || fetch(event.request);
+    })
+  );
+});
 
